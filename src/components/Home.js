5a7ab31cad2f5e8cc31eb795a40692746496e97d
bhuvaneshwari <bhuvaneshwari@gmail.com>
2023-07-04 19:42:07 +0530

    last

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Nav.css'
+const Home = () => {
+    return (
+        <div className='bgimg'>
+            <img src='bgimg.png' alt='logo' style={{ width: 1380, height: 510 }}></img></div>
+    )
+}
+
+export default Home
